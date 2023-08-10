51efc8192c2157bc6a4d15e71b36f05d5e3dd25a
Lucas <rukasubahamut@gmail.com>
2023-08-10 14:21:00 -0300

    organizing files

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,19 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './assets/index.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-
-const defaultTheme = createTheme()
 
 import App from './App'
 
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import './assets/index.css'
+import themeApp from './assets/theme'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={themeApp}>
     <CssBaseline />
     <App />
   </ThemeProvider>
