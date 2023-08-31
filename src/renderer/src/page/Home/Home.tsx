4176f575fcfe758a9f028cb86104b0aafb37449e
Troiando <jandilson.maia.jm@gmail.com>
2023-08-31 18:58:36 -0300

    MOD SeachBar

--- src/renderer/src/page/Home/Home.tsx
+++ src/renderer/src/page/Home/Home.tsx
@@ -1,12 +1,14 @@
 import { Toolbar } from '@mui/material'
 import { AppBar } from '../../components/AppBar/AppBar'
 import DrawerBar from '../../components/Drawer/DrawerBar'
+import SearchBar from '../../components/Search/SearchBar'
 
 const Home = () => {
   return (
     <AppBar>
       <Toolbar>
         <DrawerBar></DrawerBar>
+        <SearchBar />
       </Toolbar>
     </AppBar>
   )
